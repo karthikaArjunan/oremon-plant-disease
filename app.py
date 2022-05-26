889ba0ff86d9a73020c1c924c1090075ba0b14db
karthikaArjunan <71118073+karthikaArjunan@users.noreply.github.com>
2022-05-26 23:14:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,8 @@ import os
 import tensorflow as tf
 import numpy as np
 from tensorflow import keras
-import keras
 from skimage import io
 from tensorflow.keras.preprocessing import image
-from keras.preprocessing.image  import ImageDataGenerator,img_to_array, load_img
-from keras.applications.vgg19 import VGG19, preprocess_input,decode_predictions
 
 
 # Flask utils
@@ -27,15 +24,14 @@ print('Model loaded. Check http://127.0.0.1:5000/')
 
 
 def model_predict(img_path, model):
-    img = image.load_img(img_path, target_size=(256,256))
-    show_img = image.load_img(img_path,target_size=(256,256))
-    ref={0: 'Apple___Apple_scab', 1: 'Apple___Black_rot', 2: 'Apple___Cedar_apple_rust', 3: 'Apple___healthy', 4: 'Blueberry___healthy', 5: 'Cherry_(including_sour)___Powdery_mildew', 6: 'Cherry_(including_sour)___healthy', 7: 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot', 8: 'Corn_(maize)___Common_rust_', 9: 'Corn_(maize)___Northern_Leaf_Blight', 10: 'Corn_(maize)___healthy', 11: 'Grape___Black_rot', 12: 'Grape___Esca_(Black_Measles)', 13: 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 14: 'Grape___healthy', 15: 'Orange___Haunglongbing_(Citrus_greening)', 16: 'Peach___Bacterial_spot', 17: 'Peach___healthy', 18: 'Pepper,_bell___Bacterial_spot', 19: 'Pepper,_bell___healthy', 20: 'Potato___Early_blight', 21: 'Potato___Late_blight', 22: 'Potato___healthy', 23: 'Raspberry___healthy', 24: 'Soybean___healthy', 25: 'Squash___Powdery_mildew', 26: 'Strawberry___Leaf_scorch', 27: 'Strawberry___healthy', 28: 'Tomato___Bacterial_spot', 29: 'Tomato___Early_blight', 30: 'Tomato___Late_blight', 31: 'Tomato___Leaf_Mold', 32: 'Tomato___Septoria_leaf_spot', 33: 'Tomato___Spider_mites Two-spotted_spider_mite', 34: 'Tomato___Target_Spot', 
-    35: 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 36: 'Tomato___Tomato_mosaic_virus', 37: 'Tomato___healthy'}
+    img = image.load_img(img_path, grayscale=False, target_size=(64, 64))
+    show_img = image.load_img(img_path, grayscale=False, target_size=(64, 64))
     x = image.img_to_array(img)
-    im=preprocess_input(x)
-    x = np.expand_dims(im, axis=0)
-    preds = np.argmax(model.predict(x))
-    return ref[preds]
+    x = np.expand_dims(x, axis=0)
+    x = np.array(x, 'float32')
+    x /= 255
+    preds = model.predict(x)
+    return preds
 
 
 @app.route('/', methods=['GET'])
@@ -58,23 +54,19 @@ def upload():
 
         # Make prediction
         preds = model_predict(file_path, model)
-    
-        #print(preds[0])
+        print(preds[0])
 
         # x = x.reshape([64, 64]);
-        disease_class = ['Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy','Blueberry___healthy', 'Cherry_(including_sour)___Powdery_mildew', 
-        'Cherry_(including_sour)___healthy', 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot','Corn_(maize)___Common_rust_',  'Corn_(maize)___Northern_Leaf_Blight','Corn_(maize)___healthy', 
-        'Grape___Black_rot', 'Grape___Esca_(Black_Measles)','Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 'Grape___healthy',  'Orange___Haunglongbing_(Citrus_greening)', 'Peach___Bacterial_spot', 
-        'Peach___healthy','Pepper,_bell___Bacterial_spot', 'Pepper,_bell___healthy','Potato___Early_blight', 
-        'Potato___Late_blight', 'Potato___healthy', 'Raspberry___healthy', 'Soybean___healthy','Squash___Powdery_mildew',
-         'Strawberry___Leaf_scorch', 'Strawberry___healthy', 'Tomato___Bacterial_spot', 'Tomato___Early_blight',  'Tomato___Late_blight', 'Tomato___Leaf_Mold',  'Tomato___Septoria_leaf_spot', 
-        'Tomato___Spider_mites Two-spotted_spider_mite','Tomato___Target_Spot',
-        'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus', 'Tomato___healthy']
+        disease_class = ['Pepper__bell___Bacterial_spot', 'Pepper__bell___healthy', 'Potato___Early_blight',
+                         'Potato___Late_blight', 'Potato___healthy', 'Tomato_Bacterial_spot', 'Tomato_Early_blight',
+                         'Tomato_Late_blight', 'Tomato_Leaf_Mold', 'Tomato_Septoria_leaf_spot',
+                         'Tomato_Spider_mites_Two_spotted_spider_mite', 'Tomato__Target_Spot',
+                         'Tomato__Tomato_YellowLeaf__Curl_Virus', 'Tomato__Tomato_mosaic_virus', 'Tomato_healthy']
         a = preds[0]
         ind=np.argmax(a)
-        print('Prediction:', preds)
+        print('Prediction:', disease_class[ind])
         result=disease_class[ind]
-        return preds
+        return result
     return None
 
 
@@ -84,4 +76,4 @@ if __name__ == '__main__':
     # Serve the app with gevent
     http_server = WSGIServer(('', 5000), app)
     http_server.serve_forever()
-    app.run(debug=True)
+    app.run()
