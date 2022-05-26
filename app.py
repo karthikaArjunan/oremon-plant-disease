e62f1c10254067992e271dd55f4e75421c5d0723
karthikaArjunan <71118073+karthikaArjunan@users.noreply.github.com>
2022-05-26 23:10:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ app = Flask(__name__)
 # You can also use pretrained model from Keras
 # Check https://keras.io/applications/
 
-model =tf.keras.models.load_model('best_model.h5',compile=False)
+model =tf.keras.models.load_model('PlantDNet.h5',compile=False)
 print('Model loaded. Check http://127.0.0.1:5000/')
 
 
